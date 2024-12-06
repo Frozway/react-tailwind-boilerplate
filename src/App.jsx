fe3fa6bd59dab055203d12caf23f5da090262b22
Thibaut Lefrancois <thibaut.lfr@gmail.com>
2024-12-06 15:35:03 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Button } from "@/components/ui/button"
+
+function App() {
+  return (
+    <>
+      <div className="flex justify-center items-center" style={{height: '100vh'}}>
+        <h1 className="text-3xl">
+          My react tailwind boilerplate
+        </h1>
+      </div>
+    </>
+  )
+}
+
+export default App
